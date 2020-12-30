de112af119b677c56f81c916e10ca9176757f7fb
ViktorHotra <zorge.mail@gmail.com>
2020-12-30 17:42:35 +0200

    react router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { ThemeProvider } from 'styled-components';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
 import { Layout } from './components';
 import { LayoutContainer } from './containers';
@@ -6,16 +7,38 @@ import { HomePage } from './pages';
 import { GlobalStyles } from './styles';
 import { darkTheme } from './themes';
 
-export const App = () => (
-    <ThemeProvider theme={darkTheme}>
-        <GlobalStyles />
+const FakePage = props => {
+    return <p>Hello! I'm a fake page!</p>;
+};
 
-        <LayoutContainer>
-            {({ movies, ...other }) => (
-                <Layout {...other}>
-                    <HomePage movies={movies} />
-                </Layout>
-            )}
-        </LayoutContainer>
-    </ThemeProvider>
+const OfficePage = props => {
+    return <p>Hello! I'm an office page!</p>;
+};
+
+export const App = () => (
+    <BrowserRouter>
+        <ThemeProvider theme={darkTheme}>
+            <GlobalStyles />
+
+            <LayoutContainer>
+                {({ movies, ...other }) => (
+                    <Layout {...other}>
+                        <Switch>
+                            <Route path="/fake">
+                                <FakePage />
+                            </Route>
+
+                            <Route path="/" exact>
+                                <OfficePage />
+                            </Route>
+
+                            <Redirect to="/" />
+                        </Switch>
+
+                        <HomePage movies={movies} />
+                    </Layout>
+                )}
+            </LayoutContainer>
+        </ThemeProvider>
+    </BrowserRouter>
 );
